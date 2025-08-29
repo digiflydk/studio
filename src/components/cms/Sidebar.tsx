5813d9c4c6fcb485d38854c3b599315b1786e801
Omair Khan <okh2071@gmail.com>
2025-08-29 07:35:59 +0000

    admin/CMS farverne skal være - side menu i sort og resten i hvid

--- src/components/cms/Sidebar.tsx
+++ src/components/cms/Sidebar.tsx
@@ -14,8 +14,8 @@ export default function Sidebar() {
   const pathname = usePathname();
 
   return (
-    <aside className="hidden w-64 flex-col border-r bg-background sm:flex">
-      <div className="flex h-16 items-center border-b px-6">
+    <aside className="hidden w-64 flex-col border-r bg-black sm:flex text-white">
+      <div className="flex h-16 items-center border-b border-gray-800 px-6">
         <Link href="/cms">
           <Logo />
         </Link>
@@ -27,8 +27,8 @@ export default function Sidebar() {
               <Link
                 href={link.href}
                 className={cn(
-                  "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
-                  { "bg-muted text-primary": pathname === link.href }
+                  "flex items-center gap-3 rounded-lg px-3 py-2 text-gray-400 transition-all hover:text-white hover:bg-gray-800",
+                  { "bg-gray-800 text-white": pathname === link.href }
                 )}
               >
                 <link.icon className="h-4 w-4" />
