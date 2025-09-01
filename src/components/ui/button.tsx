71164178fbd3b9709d5198d269812bc1223d7944
Omair Khan <okh2071@gmail.com>
2025-09-01 14:47:47 +0000

    DF128 - du skal og øge på højden!

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,9 +21,9 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-8 py-2",
-        sm: "h-9 px-5",
-        lg: "h-11 px-10",
+        default: "h-12 px-10 py-3",
+        sm: "h-10 px-6",
+        lg: "h-14 px-12",
         icon: "h-10 w-10",
       },
     },
