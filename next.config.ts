38540753bc27321e295d54f5530851512f20a5f0
Omair Khan <okh2071@gmail.com>
2025-09-09 13:32:56 +0000

    > nextn@0.1.0 typecheck > tsc --noEmit next.config.original.ts(29,5): er

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,9 @@ const nextConfig: NextConfig = {
     ],
   },
   devIndicators: {
+    buildActivity: true,
+  },
+  experimental: {
     allowedDevOrigins: [
       'https://6000-firebase-studio-1756450523972.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev',
     ],
