3d23ab142ca11bbad4e0d67b394c92853e5f8ac6
Omair Khan <okh2071@gmail.com>
2025-09-09 07:03:12 +0000

    Restoring original next config in project root /layers/google.nodejs.fir

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -18,10 +18,10 @@ const config: Config = {
     },
     extend: {
       fontSize: {
-        'h1': ['4rem', { lineHeight: '1', fontWeight: '700' }],
-        'h2': ['2.25rem', { lineHeight: '1', fontWeight: '700' }],
-        'h3': ['1.875rem', { lineHeight: '1', fontWeight: '700' }],
-        'h4': ['1.5rem', { lineHeight: '1', fontWeight: '700' }],
+        'h1': ['4rem', { lineHeight: '1.2', fontWeight: '700' }],
+        'h2': ['2.25rem', { lineHeight: '1.2', fontWeight: '700' }],
+        'h3': ['1.875rem', { lineHeight: '1.2', fontWeight: '700' }],
+        'h4': ['1.5rem', { lineHeight: '1.2', fontWeight: '700' }],
         'body': ['1.125rem', { lineHeight: '1.75rem', fontWeight: '400' }],
       },
       colors: {
