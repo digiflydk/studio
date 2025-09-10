0babf9a13f29bf4ddb4453c54bf89c29805e1a0e
Omair Khan <okh2071@gmail.com>
2025-09-10 05:22:04 +0000

    Error: Element type is invalid: expected a string (for built-in componen

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import {cn} from '@/lib/utils';
 
-const CustomTextarea = React.forwardRef<
+const Textarea = React.forwardRef<
   HTMLTextAreaElement,
   React.ComponentProps<'textarea'>
 >(({className, ...props}, ref) => {
@@ -17,6 +17,6 @@ const CustomTextarea = React.forwardRef<
     />
   );
 });
-CustomTextarea.displayName = 'Textarea';
+Textarea.displayName = 'Textarea';
 
-export {CustomTextarea as Textarea};
+export {Textarea};
